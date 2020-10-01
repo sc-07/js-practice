6beb8eb754d81bc680d0f587c0488f5d0377a2b8
sc-07 <70569402+sc-07@users.noreply.github.com>
2020-10-01 21:07:18 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,14 @@
+
+
+function upDate(previewPic){
+ 
+  document.getElementById("image").style.backgroundImage="url(" +previewPic.src+ ")";
+  document.getElementById("image").innerHTML=previewPic.alt;
+  
+	}
+
+	function unDo(){
+     
+		document.getElementById("image").innerHTML="Hover over an image to display here.";
+     document.getElementById('image').style.backgroundImage = "url('')";
+	}
